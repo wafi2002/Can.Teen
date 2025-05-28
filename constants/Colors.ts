ff257c7cee46aa145c9a9b98281fe333ad8d5bfe
wafi2002 <wafi2002ramli@gmail.com>
2025-05-28 21:28:05 +0800

    Setup dashboard

---
+++ constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = "#19abff";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#fff",
+    tabIconDefault: "#fff",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
